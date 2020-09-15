79d06f0af745b0f422726c230f66513b7e9f75d0
Thiago de Campos <thiagocamposde@gmail.com>
2020-09-15 15:55:44 -0300

    add express-validator setup

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
-const express = require("express");
-const cors = require("cors");
-const path = require("path");
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
 const app = express();
-
 // enables cros-origin requests
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const chessRoutes = require("./routes/chess");
+const chessRoutes = require('./routes/chess');
 
 app.use(chessRoutes);
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  app.use(express.static("client/build"));
+  app.use(express.static('client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
